c0dac8b3f0461f190bb6a6b44e92203ddeec1c9f
Imad gannoun <75321420+ImadBen96@users.noreply.github.com>
2024-07-13 20:49:33 +0200

    Update database.config.ts

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,8 +1,8 @@
 import { connect ,ConnectOptions } from "mongoose";
 
 export const dbConnect = () => {
-   // const source = "mongodb+srv://dbUser:7BK5GP3XlEKgKM4r@cluster0.b1tyda0.mongodb.net/foodmine?retryWrites=true&w=majority&appName=Cluster0";
-    connect(process.env.MONGO_URI!,{
+    const source = "mongodb+srv://dbUser:7BK5GP3XlEKgKM4r@cluster0.b1tyda0.mongodb.net/foodmine?retryWrites=true&w=majority&appName=Cluster0";
+    connect(source!,{
         useNewUrlParser: true,
         useUnifiedTopology: true
     } as ConnectOptions).then(
