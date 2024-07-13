b425d7fabd7312a7db7e143deab84b5af6cf6b56
Imad gannoun <75321420+ImadBen96@users.noreply.github.com>
2024-07-13 17:04:51 +0200

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/frontend/browser/index.html'));
 });
 
-const port = 6000;
+const port = 5000;
 app.listen(port,() => {
     console.log("WebSite Served On http://localhost:"+port);
 });
