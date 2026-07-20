99a0d7cc4dce45aea1fbac382a3ef54d3b387639
ImadBen96 <ibenomar536@gmail.com>
2026-07-20 13:28:15 +0200

    change server.js

--- backend/built/server.js
+++ backend/built/server.js
@@ -23,7 +23,7 @@ app.use(express_1.default.json());
 app.use(express_1.default.static(path_1.default.join(__dirname, '../../frontend/dist/frontend/browser')));
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: ["*"]
+    origin: ["http://localhost:4200"]
 }));
 app.use("/api/foods", food_router_1.default);
 app.use("/api/users", user_router_1.default);
